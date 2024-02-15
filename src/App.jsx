c430d31ecbdde2b8557ad15ec267a3eba11cb3d2
barleviatias <barlevi.atias@gmail.com>
2024-02-15 17:51:01 +0200

    fafa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
 import './App.css';
 import Timer from './cmp/Timer';
 import YouTubeIcon from '@mui/icons-material/YouTube';
